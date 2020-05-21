6e77c5b146c239df3ecab58363f34c574dbfec6f
mssoheil <mssoheil88@yahoo.com>
2020-05-21 12:30:57 +0430

    feat: opening a url in chrome

--- examples/006/open.js
+++ examples/006/open.js
@@ -1,2 +1,24 @@
-const { platform } = require('os');
-const { exec } = require('child_process');
+const { platform } = require("os");
+const { exec } = require("child_process");
+
+const WINDOWS_PLATFORM = "win32";
+const osPlatform = platform();
+
+const args = process.argv;
+const [url] = args.slice(2);
+
+if (!url) {
+	console.log("please enter a url");
+	process.exit();
+}
+
+let command;
+
+if (osPlatform === WINDOWS_PLATFORM) {
+	command = `start chrome ${url} -incognito`;
+} else {
+	command = `open -a "Google Chrome" ${url}`;
+}
+
+console.log(`executing command ${command}`);
+exec(command);
