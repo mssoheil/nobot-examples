43623fdad9ac74fa6ffab9123b5fad03165f7589
mssoheil <mssoheil88@yahoo.com>
2020-05-21 12:13:53 +0430

    os

--- examples/005/os.js
+++ examples/005/os.js
@@ -1,1 +1,9 @@
-const os = require('os');
+const os = require("os");
+
+console.log(`home dir is ${os.homedir}`);
+console.log(`host is ${os.hostname}`);
+console.log(`platform is ${os.platform()}`);
+const cpuCores = os.cpus();
+console.log(`cpu count is ${cpuCores.length}`);
+console.log(`cpu model is ${cpuCores[0].model}`);
+console.log(`total memory is ${os.totalmem}`);
