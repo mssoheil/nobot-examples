1d74fbf4c214711d2ec42019d9eaec1fda61137a
mssoheil <mssoheil88@yahoo.com>
2020-05-25 10:34:30 +0430

    feat: colors

--- examples/009/helpers/log.js
+++ examples/009/helpers/log.js
@@ -1,20 +1,20 @@
-require('colors');
-const { ERROR, WARNING, SUCCESS } = require('../constants/message-types');
+require("colors");
+const { ERROR, WARNING, SUCCESS } = require("../constants/message-types");
 
 module.exports = function log(message, type) {
-  let colorMessage;
-  switch (type) {
-    case ERROR:
-      colorMessage = `[ERROR] ${message.red}`;
-      break;
-    case WARNING:
-      colorMessage = `[WARNING] ${message.yellow}`;
-      break;
-    case SUCCESS:
-      colorMessage = `[SUCCESS] ${message.green}`;
-      break;
-    default:
-      colorMessage = `[INFO] ${message}`;
-  }
-  console.log(colorMessage);
+	let colorMessage;
+	switch (type) {
+		case ERROR:
+			colorMessage = `[ERROR] ${message}`.brightRed;
+			break;
+		case WARNING:
+			colorMessage = `[WARNING] ${message}`.brightYellow;
+			break;
+		case SUCCESS:
+			colorMessage = `[SUCCESS] ${message}`.brightGreen;
+			break;
+		default:
+			colorMessage = `[INFO] ${message}`;
+	}
+	console.log(colorMessage);
 };
--- examples/009/log-color.js
+++ examples/009/log-color.js
@@ -1,1 +1,8 @@
-require('colors');
+require("colors");
+const log = require("./helpers/log");
+const { SUCCESS, WARNING, ERROR } = require("./constants/message-types");
+
+log("this is a success message", SUCCESS);
+log("this is a warning message", WARNING);
+log("this is an error message", ERROR);
+log("this is an info message");
