5a79f2cbb47d07616fc7c90901e5d37f768f3fcd
mssoheil <mssoheil88@yahoo.com>
2020-06-04 10:36:38 +0430

    feat: sms

--- examples/016/sms-spouse.js
+++ examples/016/sms-spouse.js
@@ -1,4 +1,23 @@
-const config = require('./config');
-const readLineSync = require('readline-sync');
-const Twilio = require('twilio');
+const config = require("./config");
+const readLineSync = require("readline-sync");
+const Twilio = require("twilio");
 
+const {
+	TWILIO_SID,
+	TWILIO_AUTH_TOKEN,
+	TWILIO_PHONE_NUMBER,
+	MY_SPOUSE_NUMBER,
+} = config;
+
+const client = new Twilio(TWILIO_SID, TWILIO_AUTH_TOKEN);
+
+const smsMessage = {
+	body: `Hi Bub, I'd like spagetti`,
+	from: TWILIO_PHONE_NUMBER,
+	to: MY_SPOUSE_NUMBER,
+};
+
+client.messages
+	.create(smsMessage)
+	.then(({ sid }) => console.log(`sms sent. id: ${sid}`))
+	.catch((err) => console.log(`error happened ${err}`));
